3aad051710c3e11a68bd01c05fac038ae6ccd962
Anas Umar H Joda <160381575+AnerzT@users.noreply.github.com>
2026-03-10 13:18:57 +0000

    FINAL V

--- server/index.ts
+++ server/index.ts
@@ -1,3 +1,7 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 import express from "express";
 import { registerRoutes } from "./routes.js"; 
 import { serveStatic } from "./static.js"; 
