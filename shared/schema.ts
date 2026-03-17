8e4331b9dbabc3bffde0ad0b36065ca2122fe129
Anas Umar H Joda <160381575+AnerzT@users.noreply.github.com>
2026-03-17 10:43:25 +0100

    Update schema.ts

--- shared/schema.ts
+++ shared/schema.ts
@@ -1,4 +1,5 @@
-import { pgTable, text, integer, timestamp, boolean, doublePrecision, jsonb, sql } from "drizzle-orm/pg-core";
+import { pgTable, text, integer, timestamp, boolean, doublePrecision, jsonb } from "drizzle-orm/pg-core";
+import { sql } from "drizzle-orm";
 import { createInsertSchema } from "drizzle-zod";
 import { z } from "zod";
 
